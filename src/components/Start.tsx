eb3d6f938a935aa0ecda1fa495a4f0bb443dd967
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2023-08-28 22:49:12 -0400

    feat: header and start component was implemented

---
+++ src/components/Start.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material';
+
+export const Start = () => {
+  return (
+    <Button variant='contained' color='primary' onClick={() => null}>
+      ¡Empezar!
+    </Button>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { darkTheme } from './components/themes/darkTheme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
